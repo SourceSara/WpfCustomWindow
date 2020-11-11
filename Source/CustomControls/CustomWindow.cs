ea4a3ee5f278dfde792640da5cdcad0448adca36
AmRo <virtualemailbox045@gmail.com>
2020-11-11 10:04:55 +0330

    Improve custom window chrome logic and UI

--- Source/CustomControls/CustomWindow.cs
+++ Source/CustomControls/CustomWindow.cs
@@ -2,26 +2,27 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace CustomControls
 {
+    [TemplatePart(Name = TemplateContainerName, Type = typeof(Border))]
+    [TemplatePart(Name = TitleBarBackgroundName, Type = typeof(Rectangle))]
+    [TemplatePart(Name = TitleBarContainerName, Type = typeof(Grid))]
+    [TemplatePart(Name = IconPresenterName, Type = typeof(Image))]
+    [TemplatePart(Name = TitlePresenterName, Type = typeof(TextBlock))]
     public class CustomWindow : Window
     {
         #region Fields
 
         private const string TemplateContainerName = "PART_TemplateContainerBorder";
+        private const string TitleBarBackgroundName = "PART_TitleBarBackgroundRectangle";
         private const string TitleBarContainerName = "PART_TitleBarContainerGrid";
         private const string IconPresenterName = "PART_IconImage";
         private const string TitlePresenterName = "PART_TitleTextBlock";
-        private const string WindowCloseButtonName = "PART_WindowCloseButton";
-        private const string WindowMaximizeButtonName = "PART_WindowMaximizeButton";
-        private const string WindowMinimizeButtonName = "PART_WindowMinimizeButton";
-
-        private Button _windowCloseButton;
-        private Button _windowMaximizeButton;
-        private Button _windowMinimizeButton;
 
         #endregion
 
@@ -33,20 +34,59 @@ namespace CustomControls
                 new FrameworkPropertyMetadata(typeof(CustomWindow)));
         }
 
+        public CustomWindow()
+        {
+            CommandBindings.Add(new CommandBinding(CloseCommand, HandleClsoeCommand));
+            CommandBindings.Add(new CommandBinding(MaximizeCommand, HandleMaximizeCommand));
+            CommandBindings.Add(new CommandBinding(MinimizeCommand, HandleMinimizeCommand));
+        }
+
+        #endregion
+
+        #region Commands
+
+        public static RoutedCommand CloseCommand = new RoutedCommand();
+        public static RoutedCommand MaximizeCommand = new RoutedCommand();
+        public static RoutedCommand MinimizeCommand = new RoutedCommand();
+
+        #endregion
+
+        #region Command Handlers
+
+        private void HandleClsoeCommand(object sender, ExecutedRoutedEventArgs e)
+            => SystemCommands.CloseWindow(this);
+
+        private void HandleMaximizeCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                SystemCommands.MaximizeWindow(this);
+            else
+                SystemCommands.RestoreWindow(this);
+        }
+
+        private void HandleMinimizeCommand(object sender, ExecutedRoutedEventArgs e)
+            => SystemCommands.MinimizeWindow(this);
+
+        #endregion
+
+        #region Properties
+
+        public FrameworkElement TemplateContainer => GetTemplateChild(TemplateContainerName) as Border;
+
         #endregion
 
         #region Overrides
 
         public override void OnApplyTemplate()
         {
-            UnregisterClickHandlers();
-            LoadButtonsTemplate();
-            RegisterClickHandlers();
+            base.OnApplyTemplate();
             CheckIconAndTitle();
         }
 
         protected override void OnStateChanged(EventArgs e)
         {
+            base.OnStateChanged(e);
+
             if (!(GetTemplateChild(TemplateContainerName) is Border templateContainerBorder))
                 return;
 
@@ -71,7 +111,7 @@ namespace CustomControls
 
         #endregion
 
-        #region Methods
+        #region Helper methods
 
         protected virtual bool IsTaskbarVisible()
             => NativeMethods.GetTaskbarState() != NativeMethods.TaskbarStates.AutoHide;
@@ -97,73 +137,30 @@ namespace CustomControls
 
             // NOTE:
             //  To adjust window position when auto-hide taskbar enabled. 
-            //  If we don't remove one pixel from bottom of the window,
+            //  If we don't remove one pixel from height of the window,
             //  our custom window will block auto-hide taskbar.
+            //  Not works properly when GlassFrameThickness set to 0
             height = removeOnePixel ? height - 1 : height;
 
             NativeMethods.SetWindowPos(currentWindowHandle, 0, x, y, width, height, NativeMethods.SwpShowWindow);
         }
 
-        protected virtual void LoadButtonsTemplate()
-        {
-            _windowCloseButton = (Button)GetTemplateChild(WindowCloseButtonName)
-                ?? throw new NullReferenceException("Close button template not found.");
-
-            _windowMinimizeButton = (Button)GetTemplateChild(WindowMinimizeButtonName)
-                ?? throw new NullReferenceException("Minimize button template not found.");
-
-            _windowMaximizeButton = (Button)GetTemplateChild(WindowMaximizeButtonName)
-                ?? throw new NullReferenceException("Maximize button template not found.");
-        }
-
-        protected virtual void RegisterClickHandlers()
-        {
-            _windowCloseButton.Click += CloseWindow;
-            _windowMinimizeButton.Click += MinimizeWindow;
-            _windowMaximizeButton.Click += MaximizeWindow;
-        }
-
-        protected virtual void UnregisterClickHandlers()
-        {
-            if (_windowCloseButton != null)
-                _windowCloseButton.Click -= CloseWindow;
-
-            if (_windowMinimizeButton != null)
-                _windowMinimizeButton.Click -= MinimizeWindow;
-
-            if (_windowMaximizeButton != null)
-                _windowMaximizeButton.Click -= MaximizeWindow;
-        }
-
         protected virtual void CheckIconAndTitle()
         {
-            var TitleBarContainer = GetTemplateChild(TitleBarContainerName) as Grid;
+            var titlebarContainer = GetTemplateChild(TitleBarContainerName) as Grid;
 
             if (Icon == null)
             {
                 var iconImage = GetTemplateChild(IconPresenterName) as Image;
-                TitleBarContainer?.Children.Remove(iconImage);
+                titlebarContainer?.Children.Remove(iconImage);
             }
 
             if (string.IsNullOrWhiteSpace(Title))
             {
                 var titleTextBlock = GetTemplateChild(TitlePresenterName) as TextBlock;
-                TitleBarContainer?.Children.Remove(titleTextBlock);
+                titlebarContainer?.Children.Remove(titleTextBlock);
             }
         }
-
-        #endregion
-
-        #region Event Handlers
-
-        protected virtual void CloseWindow(object sender, EventArgs args)
-            => Close();
-
-        protected virtual void MinimizeWindow(object sender, EventArgs args)
-            => WindowState = WindowState.Minimized;
-
-        protected virtual void MaximizeWindow(object sender, EventArgs args)
-            => WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
 
         #endregion
 
@@ -171,7 +168,7 @@ namespace CustomControls
 
         public static readonly DependencyProperty ActiveTitleBarBackgroundProperty =
             DependencyProperty.Register(nameof(ActiveTitleBarBackground),
-                typeof(Brush), typeof(Window),
+                typeof(Brush), typeof(CustomWindow),
                 new PropertyMetadata(default(Brush)));
 
         public Brush ActiveTitleBarBackground
@@ -186,7 +183,7 @@ namespace CustomControls
 
         public static readonly DependencyProperty InactiveTitleBarBackgroundProperty =
             DependencyProperty.Register(nameof(InactiveTitleBarBackground),
-                typeof(Brush), typeof(Window),
+                typeof(Brush), typeof(CustomWindow),
                 new PropertyMetadata(default(Brush)));
 
         public Brush InactiveTitleBarBackground
@@ -197,32 +194,47 @@ namespace CustomControls
 
         #endregion
 
-        #region DependencyProperty : WindowButtonMouseOverBackground
+        #region DependencyProperty : ButtonForeground
 
-        public static readonly DependencyProperty WindowButtonMouseOverBackgroundProperty =
-            DependencyProperty.Register(nameof(WindowButtonMouseOverBackground),
-                typeof(Brush), typeof(Window),
+        public static readonly DependencyProperty ButtonForegroundProperty =
+            DependencyProperty.Register(nameof(ButtonForeground),
+                typeof(Brush), typeof(CustomWindow),
                 new PropertyMetadata(default(Brush)));
 
-        public Brush WindowButtonMouseOverBackground
+        public Brush ButtonForeground
         {
-            get => (Brush)GetValue(WindowButtonMouseOverBackgroundProperty);
-            set => SetValue(WindowButtonMouseOverBackgroundProperty, value);
+            get => (Brush)GetValue(ButtonForegroundProperty);
+            set => SetValue(ButtonForegroundProperty, value);
         }
 
         #endregion
 
-        #region DependencyProperty : WindowButtonMouseOverForeground
+        #region DependencyProperty : ButtonMouseOverBackground
 
-        public static readonly DependencyProperty WindowButtonMouseOverForegroundProperty =
-            DependencyProperty.Register(nameof(WindowButtonMouseOverForeground),
-                typeof(Brush), typeof(Window),
+        public static readonly DependencyProperty ButtonMouseOverBackgroundProperty =
+            DependencyProperty.Register(nameof(ButtonMouseOverBackground),
+                typeof(Brush), typeof(CustomWindow),
                 new PropertyMetadata(default(Brush)));
 
-        public Brush WindowButtonMouseOverForeground
+        public Brush ButtonMouseOverBackground
         {
-            get => (Brush)GetValue(WindowButtonMouseOverForegroundProperty);
-            set => SetValue(WindowButtonMouseOverForegroundProperty, value);
+            get => (Brush)GetValue(ButtonMouseOverBackgroundProperty);
+            set => SetValue(ButtonMouseOverBackgroundProperty, value);
+        }
+
+        #endregion
+
+        #region DependencyProperty : ButtonMouseOverForeground
+
+        public static readonly DependencyProperty ButtonMouseOverForegroundProperty =
+            DependencyProperty.Register(nameof(ButtonMouseOverForeground),
+                typeof(Brush), typeof(CustomWindow),
+                new PropertyMetadata(default(Brush)));
+
+        public Brush ButtonMouseOverForeground
+        {
+            get => (Brush)GetValue(ButtonMouseOverForegroundProperty);
+            set => SetValue(ButtonMouseOverForegroundProperty, value);
         }
 
         #endregion
